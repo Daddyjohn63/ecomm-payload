d26744f04553b9538484f17f1ce7d6502a5dc055
Daddyjohn63 <john@webandprosper.co.uk>
2025-05-16 19:15:36 +0100

    seed data

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -1,6 +1,6 @@
 'use client';
 // ^-- to make sure we can mount the Provider from a server component
-import superjson from "superjson";
+import superjson from 'superjson';
 import type { QueryClient } from '@tanstack/react-query';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { createTRPCClient, httpBatchLink } from '@trpc/client';
@@ -8,6 +8,7 @@ import { createTRPCContext } from '@trpc/tanstack-react-query';
 import { useState } from 'react';
 import { makeQueryClient } from './query-client';
 import type { AppRouter } from './routers/_app';
+
 export const { TRPCProvider, useTRPC } = createTRPCContext<AppRouter>();
 let browserQueryClient: QueryClient;
 function getQueryClient() {
@@ -32,7 +33,7 @@ function getUrl() {
 export function TRPCReactProvider(
   props: Readonly<{
     children: React.ReactNode;
-  }>,
+  }>
 ) {
   // NOTE: Avoid useState when initializing the query client if you don't
   //       have a suspense boundary between this and the code that may
@@ -44,10 +45,10 @@ export function TRPCReactProvider(
       links: [
         httpBatchLink({
           transformer: superjson,
-          url: getUrl(),
-        }),
-      ],
-    }),
+          url: getUrl()
+        })
+      ]
+    })
   );
   return (
     <QueryClientProvider client={queryClient}>
